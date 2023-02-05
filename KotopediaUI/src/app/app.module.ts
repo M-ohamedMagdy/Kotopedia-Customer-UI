d15378e983cb342c28227923881b416913174c9d
menna sobeh <mennasopeh@gmail.com>
2023-02-05 10:54:04 +0200

    aboutPlusfooter

--- KotopediaUI/src/app/app.module.ts
+++ KotopediaUI/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { AboutComponent } from './components/partials/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/partials/footer/footer.component';
+// import {MatGridListModule} from '@angular/material';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // MatGridListModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
