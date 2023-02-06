5e25804bfc6258341728265bf662c4ab1e2ca921
Mahamed Magdy <m7md96mgdy@gmail.com>
2023-02-06 12:23:44 +0200

    Merge branch 'magdy'

--- KotopediaUI/src/app/app-routing.module.ts
+++ KotopediaUI/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterationComponent } from './components/pages/registeration/registeration.component';
+import { AboutComponent } from './components/partials/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+                          {path:"about",component:AboutComponent},
+                          {path:"login", component:LoginComponent},
+                          {path:"signup", component:RegisterationComponent},
+                       ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- KotopediaUI/src/app/app.module.ts
+++ KotopediaUI/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from "./components/partials/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+
+    ]
 })
 export class AppModule { }
