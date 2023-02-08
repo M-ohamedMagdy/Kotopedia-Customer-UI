e8fe83a3769711667e9a348d579b66397da98da5
Abanoub Saleh <abanoubsaleh56@gmail.com>
2023-02-08 15:31:48 +0200

    cart added

---
+++ KotopediaUI/src/app/components/pages/cart/cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+x:number=1;
+price:number=this.x*44;
+}
